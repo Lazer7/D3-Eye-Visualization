d895d08faa935f5ee0bb85f8fd04dc6d1fe7ac9a
Jose <gallardojose444@gmail.com>
2019-10-02 15:02:29 -0700

    Filtered Data for Testing

---
+++ Data/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+var dataGZD = require('./testingtreeGZD');
+var dataFXD = require('./testingtreeFXD');
+
+var counter = 0;
+var total = 0;
+var final = [];
+//10 seconds of data
+var timeMax = 100000
+dataGZD.forEach(elementGZD => {
+    dataFXD.forEach(elementFXD => {
+      if( elementFXD.Time == elementGZD.Time){final.push(elementFXD); counter++;}
+    })
+    // if( element.Time < timeMax){final.push(element); counter++;}
+    // if (element.LeftCode < 2 &&
+    //     element.RightCode < 2 &&
+    //     element.LeftScreenX > -1000 &&
+    //     element.RightScreenX > -1000 &&
+    //     element.LeftScreenY > -1000 &&
+    //     element.RightScreenY > -1000 &&
+    //     element.Time < timeMax) {
+    //     counter++;
+    //     final.push(element)
+    // }
+});
+
+fs.writeFile("testingtreeFXD.js", JSON.stringify(final), function (err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
+
+
+console.log(counter);
+console.log(total)
