4c77bc83afc4fc7d5333ea8b0d06bd531c26a98a
Jose <gallardojose444@gmail.com>
2019-10-02 15:57:38 -0700

    changed filtering

--- Data/index.js
+++ Data/index.js
@@ -1,27 +1,27 @@
 const fs = require('fs')
 var dataGZD = require('./testingtreeGZD');
-var dataFXD = require('./testingtreeFXD');
+var dataFXD = require('./');
 
 var counter = 0;
 var total = 0;
 var final = [];
 //10 seconds of data
 var timeMax = 100000
+
 dataGZD.forEach(elementGZD => {
-    dataFXD.forEach(elementFXD => {
-      if( elementFXD.Time == elementGZD.Time){final.push(elementFXD); counter++;}
-    })
+    // dataFXD.forEach(elementFXD => {
+    //   if( elementFXD.Time == elementGZD.Time){final.push(elementFXD); counter++; console.log() }
+    // })
     // if( element.Time < timeMax){final.push(element); counter++;}
-    // if (element.LeftCode < 2 &&
-    //     element.RightCode < 2 &&
-    //     element.LeftScreenX > -1000 &&
-    //     element.RightScreenX > -1000 &&
-    //     element.LeftScreenY > -1000 &&
-    //     element.RightScreenY > -1000 &&
-    //     element.Time < timeMax) {
-    //     counter++;
-    //     final.push(element)
-    // }
+    if (element.LeftCode < 2 &&
+        element.RightCode < 2 &&
+        element.LeftScreenX > -1000 &&
+        element.RightScreenX > -1000 &&
+        element.LeftScreenY > -1000 &&
+        element.RightScreenY > -1000) {
+        counter++;
+        final.push(element)
+    }
 });
 
 fs.writeFile("testingtreeFXD.js", JSON.stringify(final), function (err) {
