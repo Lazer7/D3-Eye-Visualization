07d94f864104398e3f47dcee567b31356e25d11a
Jose Gallardo <gallardojose444@gmail.com>
2019-10-03 19:22:11 -0700

    Finished updating

--- Data/index.js
+++ Data/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
-var dataGZD = require('./p6treeFXD');
-//var dataFXD = require('./');
+var data = require('./p6treeGZD');
 
 var counter = 0;
 var total = 0;
@@ -8,21 +7,19 @@ var final = [];
 //10 seconds of data
 var timeMax = 100000
 
-dataGZD.forEach(element => {
-    // dataFXD.forEach(elementFXD => {
-    //   if( elementFXD.Time == elementGZD.Time){final.push(elementFXD); counter++; console.log() }
-    // })
-     if( element.Time < timeMax){final.push(element); counter++;}
-    //if (element.leftcode < 2 &&
-    //    element.rightcode < 2 &&
-    //    element.leftscreenx > -1000 &&
-    //    element.rightscreenx > -1000 &&
-    //    element.leftscreeny > -1000 &&
-    //    element.rightscreeny > -1000 &&
-    //    element.time < timemax) {
-    //    counter++;
-    //    final.push(element)
-    //}
+data.forEach(element => {
+    total++;
+    //if( element.Time < timeMax){final.push(element); counter++;}
+    if (element.LeftCode < 2 &&
+        element.RightCode < 2 &&
+        element.LeftScreenX > -1000 &&
+        element.RightScreenX > -1000 &&
+        element.LeftScreenY > -1000 &&
+        element.RightScreenY > -1000 &&
+        element.Time < timeMax) {
+        counter++;
+        final.push(element)
+    }
 });
 
 fs.writeFile("testingtreeGZD.js", JSON.stringify(final), function (err) {
