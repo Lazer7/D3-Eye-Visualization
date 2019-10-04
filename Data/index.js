c58569bd999b0924a8875d0ddbadbf7875fd7e18
Jose Gallardo <gallardojose444@gmail.com>
2019-10-03 19:19:00 -0700

    Updated data to test
    testingtreeFXD.js, testingtreeGZD.js

--- Data/index.js
+++ Data/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
-var dataGZD = require('./testingtreeGZD');
-var dataFXD = require('./');
+var dataGZD = require('./p6treeFXD');
+//var dataFXD = require('./');
 
 var counter = 0;
 var total = 0;
@@ -8,23 +8,24 @@ var final = [];
 //10 seconds of data
 var timeMax = 100000
 
-dataGZD.forEach(elementGZD => {
+dataGZD.forEach(element => {
     // dataFXD.forEach(elementFXD => {
     //   if( elementFXD.Time == elementGZD.Time){final.push(elementFXD); counter++; console.log() }
     // })
-    // if( element.Time < timeMax){final.push(element); counter++;}
-    if (element.LeftCode < 2 &&
-        element.RightCode < 2 &&
-        element.LeftScreenX > -1000 &&
-        element.RightScreenX > -1000 &&
-        element.LeftScreenY > -1000 &&
-        element.RightScreenY > -1000) {
-        counter++;
-        final.push(element)
-    }
+     if( element.Time < timeMax){final.push(element); counter++;}
+    //if (element.leftcode < 2 &&
+    //    element.rightcode < 2 &&
+    //    element.leftscreenx > -1000 &&
+    //    element.rightscreenx > -1000 &&
+    //    element.leftscreeny > -1000 &&
+    //    element.rightscreeny > -1000 &&
+    //    element.time < timemax) {
+    //    counter++;
+    //    final.push(element)
+    //}
 });
 
-fs.writeFile("testingtreeFXD.js", JSON.stringify(final), function (err) {
+fs.writeFile("testingtreeGZD.js", JSON.stringify(final), function (err) {
     if (err) {
         return console.log(err);
     }
